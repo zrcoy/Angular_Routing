c13f8c31ee8c2911aff4b5d4c24eca51dbf92366
zrcoy <zhangruichaotyler@gmail.com>
2021-05-22 16:20:45 -0400

    passing and retrieving query parameters and fragments, setting up child routes, configuring the handling of query parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,20 @@ import { UserComponent } from "./users/user/user.component";
 
 const appRoute: Routes = [
   { path: "", component: HomeComponent },
-  { path: "users", component: UsersComponent },
-  { path: "users/:id/:name", component: UserComponent },
-  { path: "servers", component: ServersComponent },
-  { path: "servers/:id/edit", component: EditServerComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
+  },
+
+  {
+    path: "servers",
+    component: ServersComponent,
+    children: [
+      { path: ":id", component: ServerComponent },
+      { path: ":id/edit", component: EditServerComponent },
+    ],
+  },
 ];
 
 @NgModule({
